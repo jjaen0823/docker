7f634e231652dddd8dec5289019b561ffb1b3e09
ChoiJaeEun <poungki0823@gmail.com>
2022-02-14 01:29:56 +0900

    docker practice

---
+++ Gugudan.java
@@ -0,0 +1,14 @@
+import java.net.http.HttpResponse;
+
+public class Gugudan {
+    public static void main(String[] args) {
+        for (int i = 2; i < 10; i++) {
+            System.out.println(i + " Dan");
+
+            for (int j = 1; j < 10; j++) {
+                System.out.println(i + " x " + j + " = " + i * j);
+            }
+            System.out.println();
+        }
+    }
+}
